d4b32b15c88bfcb55b5bd8f0f31eaa6356da59d1
Jen Poole <contact.vandepool@gmail.com>
2018-01-31 19:24:32 +0000

    Done.

--- custom_errors.rb
+++ custom_errors.rb
@@ -19,3 +19,14 @@ puts beyonce.name
 
 
 
+# ruby custom_errors.rb
+
+=begin
+
+  Notes
+  - get NoMethodError if try to call #get_married method with an argument that is not an instance of the Person class
+  - example: beyonce.get_married("Jay-Z") --> "Jay-Z" is a String, NOT an instance of the Person class...
+    ...result --> `get_married': undefined method `partner=' for "Jay-Z":String (NoMethodError)
+
+  
+=end
