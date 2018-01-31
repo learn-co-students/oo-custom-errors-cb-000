c3e9c1479ab9952681fdfc668205147c6e9f944e
Jen Poole <contact.vandepool@gmail.com>
2018-01-31 19:27:59 +0000

    Done.

--- custom_errors.rb
+++ custom_errors.rb
@@ -10,6 +10,10 @@ class Person
     person.partner = self
   end
 
+  # define a custom error class, PartnerError, that inherits from StandardError
+  class PartnerError < StandardError
+  end
+
 end
 
 beyonce = Person.new("Beyonce")
@@ -28,5 +32,9 @@ puts beyonce.name
   - example: beyonce.get_married("Jay-Z") --> "Jay-Z" is a String, NOT an instance of the Person class...
     ...result --> `get_married': undefined method `partner=' for "Jay-Z":String (NoMethodError)
 
-  
+  Where does a Custom Error Class belong?
+  - it can be defined inside the Person class
+  - it can be defined outside the Person class
+  - it can be defined in a module which is then included inside the Person class
+
 =end
